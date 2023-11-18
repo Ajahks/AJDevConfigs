1cfde959b4564a82f899f75b56dc208f66949ea1
Ajahks <arrendeman@gmail.com>
2023-11-17 22:11:20 -0800

    Make LSP auto complete with ENTER and preselect first option.  Also make code actions ALT + ENTER

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,6 +4,7 @@ lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
   lsp_zero.default_keymaps({buffer = bufnr})
+  vim.keymap.set('n', '<A-CR>', '<cmd>lua vim.lsp.buf.code_action()<cr>', {buffer = bufnr})
 end)
 
 require('mason').setup({})
@@ -17,6 +18,19 @@ require('mason-lspconfig').setup({
   },
   handlers = {
     lsp_zero.default_setup,
+  },
+})
+
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+  }),
+
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
   },
 })
 
