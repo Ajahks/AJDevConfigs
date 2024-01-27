0ae536445536bdb536b13d3fc373a0c7cae1dec9
Arren <arrendeman@gmail.com>
2024-01-26 21:27:01 -0800

    Add packer setup to setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 # Nvim config setup
 ln -s ~/AJDevConfigs/nvim ~/.config/nvim
+
+# Install Packer
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
