01b6d358bd2f20dad80e5ac9f650529534214d69
Arren De Manuel <arrendeman@gmail.com>
2024-10-20 12:50:40 -0700

    Set custom tabs for javascript and typescript

--- nvim/lua/ajahks/set.lua
+++ nvim/lua/ajahks/set.lua
@@ -6,6 +6,19 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.api.nvim_create_augroup("CustomTabSettings", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "javascript,typescript,typescriptreact,javascriptreact",
+  group = "CustomTabSettings",
+  callback = function()
+    vim.opt.tabstop = 2
+    vim.opt.softtabstop = 2
+    vim.opt.shiftwidth = 2
+    vim.opt.expandtab = true
+  end,
+})
+
 vim.opt.incsearch = true
 
 vim.opt.scrolloff = 8
